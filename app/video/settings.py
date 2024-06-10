46d7bf4ecf3e58c3c35b8d5869d03dbb87c7d424
hodzhakhov <hodzhakhov@gmail.com>
2024-06-10 18:15:08 +0300

    Fix host

--- app/video/settings.py
+++ app/video/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "foo"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 1
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
